e96c83eee08c0859e0924727c3d82e321c81dc9c
Diogo S <dooss2085@gmail.com>
2025-05-20 16:44:38 -0300

    Criação do frontend da tela de criar produtos do restaurante

--- src/src/pages/Restaurante/CriarProduto/CriarProduto.jsx
+++ src/src/pages/Restaurante/CriarProduto/CriarProduto.jsx
@@ -5,6 +5,18 @@ function CriarProduto() {
     <>
       <CabecalhoRestaurante/>
       <h1 className={styles.titulo}>Criar Produto</h1>
+      <p className={styles.instrucao} >Preencha o formulário abaixo para adicionar <br /> um produto ao seu catálogo</p>
+
+      <div className={styles.inputs}>
+        <input type="text" placeholder="Nome" className={styles.texto}/>
+        <input type="text" placeholder="Descrição" className={styles.texto} />
+        <input type="text" placeholder="URL de uma foto" className={styles.texto}/>
+        <input type="number" placeholder="Preço" className={styles.texto}/>
+        <button type="button" className={styles.criar}>Criar</button>
+      </div>
+
+      
+
     </>
   );
 }
