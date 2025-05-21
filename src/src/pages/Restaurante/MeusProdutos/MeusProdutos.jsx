ffffadfc9945ca0d16fef1fd1cbeee75e36e497f
Seu Nome <caue.luz14@gmail.com>
2025-05-20 21:01:10 -0300

    Página de MeusProdutos

--- src/src/pages/Restaurante/MeusProdutos/MeusProdutos.jsx
+++ src/src/pages/Restaurante/MeusProdutos/MeusProdutos.jsx
@@ -1,10 +1,11 @@
 import styles from "./MeusProdutos.module.css";
 import CabecalhoRestaurante from "../../../components/CabecalhoRestaurante/CabecalhoRestaurante.jsx";
+import Funcao from "D:/programação/projetoA32/EntregaP1A3/src/src/pages/Restaurante/MeusProdutos/Funcao.jsx"
 function MeusProdutos() {
   return (
     <>
       <CabecalhoRestaurante/>
-      <h1 className={styles.titulo}>Meus Produtos</h1>
+      <Funcao />
     </>
   );
 }
